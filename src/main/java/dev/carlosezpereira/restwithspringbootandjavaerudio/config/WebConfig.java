e715b132905f9bcade9d4ff8db28c55cd391fa3c
Carlos Eduardo Zancanella Pereira <84425418+Zanca03@users.noreply.github.com>
2024-02-17 18:32:34 -0300

    Add some coments

--- src/main/java/dev/carlosezpereira/restwithspringbootandjavaerudio/config/WebConfig.java
+++ src/main/java/dev/carlosezpereira/restwithspringbootandjavaerudio/config/WebConfig.java
@@ -8,9 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+//        Query Parameter
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+//        Header Parameter
+                configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
